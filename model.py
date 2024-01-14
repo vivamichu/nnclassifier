d931bd7d6b14e296d238db001953fe5f311c85e3
Ayaulym <92267183+vivamichu@users.noreply.github.com>
2024-01-14 12:08:27 +0600

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-# model.py
-
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -8,20 +6,19 @@ class FlowerClassifier(nn.Module):
     def __init__(self, num_classes, hidden_units=4096):
         super(FlowerClassifier, self).__init__()
 
-        # Load the pre-trained VGG16 model
+        # Loading the model
         vgg16 = models.vgg16(pretrained=True)
 
-        # Freeze the pre-trained layers
+        # Freezing the model
         for param in vgg16.features.parameters():
             param.requires_grad = False
 
-        # Extract the features (excluding the classifier)
+        # Extracting the features (excluding the classifier)
         self.features = vgg16.features
 
-        # Calculate the number of features after the convolutional layers
         num_conv_features = self._get_num_conv_features()
 
-        # Add a new classifier with customizable hidden units
+        # new classifier
         self.classifier = nn.Sequential(
             nn.Linear(num_conv_features, hidden_units),
             nn.ReLU(),
@@ -35,12 +32,11 @@ class FlowerClassifier(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)  # Flatten the convolutional features
+        x = x.view(x.size(0), -1)  
         x = self.classifier(x)
         return x
 
     def _get_num_conv_features(self):
-        # Test with a dummy input to calculate the number of features
         with torch.no_grad():
             dummy_input = torch.zeros(1, 3, 224, 224)
             features = self.features(dummy_input)
